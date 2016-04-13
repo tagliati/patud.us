70a231822ceea197c0a024281f2c8caec14884d1
tagliati <andre.tagliati@netshoes.com>
2016-04-13 19:10:57 -0300

    Add new field

--- migrations.js
+++ migrations.js
@@ -3,7 +3,7 @@ var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database(config.db);
 
 db.serialize(function(){
-	db.run("CREATE TABLE if not exists pet (name TEXT,slug TEXT,description TEXT,age INTEGER)");
+	db.run("CREATE TABLE if not exists pet (name TEXT,slug TEXT,description TEXT,age INTEGER,picture TEXT)");
 	db.run("CREATE TABLE if not exists owner (name TEXT,phone TEXT,email TEXT)");
 });
 db.close();
